5f83d8c6be2776ad73d81f3b695e73f15214ac6a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 18:23:13 +0330

    add entities to db context

--- DataBase/Context/DbContext.cs
+++ DataBase/Context/DbContext.cs
@@ -1,7 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataBase.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataBase.Context;
 
 public class GraphContext : DbContext
 {
+	public GraphContext(DbContextOptions<GraphContext> options) : base(options)
+	{
+
+	}
+
+	public virtual DbSet<Group> Group { get; set; }
+
+	public virtual DbSet<Product> Product { get; set; }
 }
