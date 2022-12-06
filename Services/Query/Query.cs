1d8bb5cc0a9bd79c610f82ebcf6f1d976e93a20f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 20:06:30 +0330

    Create Query.cs

---
+++ Services/Query/Query.cs
@@ -0,0 +1,17 @@
+﻿using DataBase.Context;
+using DataBase.Entities;
+
+namespace Services.Query;
+
+public class Query
+{
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Group> GetGroups([Service] GraphContext graphContext) => graphContext.Group;
+
+    [UseProjection]
+    [UseFiltering]
+    [UseSorting]
+    public IQueryable<Product> GetProducts([Service] GraphContext graphContext) => graphContext.Product;
+}
