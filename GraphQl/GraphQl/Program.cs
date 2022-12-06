569416fc8155a00e811e04fa32645ccea7895df4
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 18:24:36 +0330

    Update Program.cs

--- GraphQl/GraphQl/Program.cs
+++ GraphQl/GraphQl/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
