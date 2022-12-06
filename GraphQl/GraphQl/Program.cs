e44f43000f47ee596827e85d23fb97899a0ed900
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 18:04:19 +0330

    Update Program.cs

--- GraphQl/GraphQl/Program.cs
+++ GraphQl/GraphQl/Program.cs
@@ -1,3 +1,5 @@
+using Services.Injection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
@@ -21,5 +24,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGraphQL("/graphql");
 
 app.Run();
