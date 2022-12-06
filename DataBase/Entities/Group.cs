cd044958f62ec128f4bcdd00fb3438734a6bb9e8
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 18:22:10 +0330

    create entities

---
+++ DataBase/Entities/Group.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataBase.Entities;
+
+public record Group
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    //Navigation Properties
+    //Relationships
+
+    public virtual ICollection<Product> Products { get; set; }
+}
---
+++ DataBase/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataBase.Entities;
+
+public record Product
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
+    public float Amount { get; set; }
+
+    [Required]
+    public Guid GroupId { get; set; }
+
+    //Navigation Properties
+    //Relationships
+
+    public virtual Group Group { get; set; }
+}
