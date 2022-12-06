38b1c00c4d165ced990c1e7a6e0e4f09be43fb07
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-12-06 18:24:38 +0330

    Update Injector.cs

--- Services/Injection/Injector.cs
+++ Services/Injection/Injector.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using DataBase.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Services.Injection;
 
 public static class ProgramInjections
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
     {
+
+        //Db Context
+        services.AddDbContext<GraphContext>(options => options.UseSqlServer(configuration.GetConnectionString("GraphDb")));
+
+        //Db Services
+
         services.AddGraphQLServer();
+
+
 
         return services;
     }
